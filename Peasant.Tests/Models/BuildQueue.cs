5956e465285c40aa3efc6d83ebaed62d903b4436
Paul Betts <paul@paulbetts.org>
2013-11-21 12:45:30 -0800

    Add some stubbed tests

---
+++ Peasant.Tests/Models/BuildQueue.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Peasant.Models.Tests
+{
+    public class BuildQueueTests
+    {
+        [Fact]
+        public void FullBuildIntegrationTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void BuildsThatFailShouldBeRecorded()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void BuildsThatSucceedShouldBeRecorded()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void ProcessSingleBuildIntegrationTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void PausingTheQueueShouldntLoseBuilds()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
