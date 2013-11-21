306038897ab7b78e95a0117ecabec76506ebb55d
Paul Betts <paul@paulbetts.org>
2013-11-21 12:55:11 -0800

    Add a build script

---
+++ script/cibuild.ps1
@@ -0,0 +1,9 @@
+$MSBuild = "C:\Program Files (x86)\MSBuild\12.0\bin\MSBuild.exe"
+
+$SlnFileExists = Test-Path ".\Peasant.sln"
+if ($SlnFileExists -eq $False) {
+    echo "*** ERROR: Run this in the project root ***"
+    exit -1
+}
+
+& "$MSBuild" /t:Rebuild /p:Configuration=Release .\Peasant.sln
