2a5ecbb69feac0216c5e1ce714e992bacebf861b
Paul Betts <paul@paulbetts.org>
2013-11-21 16:24:52 -0800

    Add a Subject that tracks the current aggregate for build outputs

---
+++ Peasant/Helpers/AggregateStringSubject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peasant.Helpers
+{
+    public class AggregateStringSubject : ISubject<string, string>
+    {
+        readonly StringBuilder buffer = new StringBuilder();
+        readonly Subject<string> inner = new Subject<string>();
+
+        public AggregateStringSubject()
+        {
+            inner.Subscribe(x => buffer.AppendLine(x));
+        }
+
+        public void OnCompleted() { inner.OnCompleted(); }
+        public void OnError(Exception error) { inner.OnError(error); }
+        public void OnNext(string value) { inner.OnNext(value); }
+
+        public string Current {
+            get { return buffer.ToString(); }
+        }
+
+        public IDisposable Subscribe(IObserver<string> observer)
+        {
+            return inner.Subscribe(observer);
+        }
+    }
+}
