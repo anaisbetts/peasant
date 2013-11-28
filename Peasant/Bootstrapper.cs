e0f0a1bf8822eab0a1360f8f0f2c1f290a32dddf
Paul Betts <paul@paulbetts.org>
2013-11-27 17:17:13 -0800

    Grab status scope from token

--- Peasant/Bootstrapper.cs
+++ Peasant/Bootstrapper.cs
@@ -5,6 +5,7 @@
     using Nancy.Session;
     using Nancy.SimpleAuthentication;
     using Peasant.Helpers;
+    using Peasant.Models;
     using SimpleAuthentication.Core;
     using SimpleAuthentication.ExtraProviders;
     using System;
@@ -27,7 +28,7 @@
             var githubAuth = new GitHubProvider(new ProviderParams() {
                 PublicApiKey = Environment.GetEnvironmentVariable("PEASANT_OAUTH_ID") ?? "68e704a8ede918f8d940",
                 SecretApiKey = Environment.GetEnvironmentVariable("PEASANT_OAUTH_KEY") ?? "888b305d22b2d7251b087c5903be21f7eaca4e20",
-                Scopes = new[] { "repo", "user", }
+                Scopes = new[] { "repo", "user", "status", }
             });
 
             var authServiceFactory = new AuthenticationProviderFactory();
